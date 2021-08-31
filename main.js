11fa363fbaa16596eeb5ee7c2097a990481100e8
aabir2011 <80570715+aabir2011@users.noreply.github.com>
2021-08-31 16:12:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true, video:false});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/248t6FTUU/.json', {
+      probabilityThreshold: 0.7 } , modelReady);
+
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+var Meowing = 0;
+var Bleating = 0;
+var Mooing = 0;
+var Barking = 0;
+
+
+function gotResults(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      random_number_r = Math.floor(Math.random() * 255) + 1;
+      random_number_g = Math.floor(Math.random() * 255) + 1;
+      random_number_b = Math.floor(Math.random() * 255) + 1;
+      
+      document.getElementById("result_label").innerHTML = 'Detected Voice is of -  '+ results[0].label;
+      document.getElementById("result_label").innerHTML = 'Detected Dog 🐶 -  '+dog+ ' Detected Cat 🐈 - ' +cat+  ' Detected Cow 🐮  - ' +cow+  ' Detected Ram/Goat 🐐 - ' +goat;
+      document.getElementById("result_label").style.color = 
+      "rgb("+random_number_r+","+randomnumber__g+","+random_number_r+")";
+      document.getElementById("result_count").style.color = 
+      "rgb("+random_number_r+","+randomnumber__g+","+random_number_r+")";
+      img = document.getElementById('animal_image');
+
+      if (results[0].label == "Barking")
+{
+  img.src = 'Dog.gif';
+  dog = dog+1;
+}
+else if (results[0].label == "Meowing")
+{
+  img.src = 'cat.gif';
+  cat = cat+1;
+
+}
+else if (results[0].label == "Bleating")
+{
+  img.src = 'sheep.gif';
+  sheep = sheep+1;
+
+}
+else if (results[0].label == "Mooing")
+{
+  img.src = 'cow.gif';
+  cow = cow+1;
+
+}
+else 
+{
+  img.src = 'Ear.gif';
+}
+}  }
